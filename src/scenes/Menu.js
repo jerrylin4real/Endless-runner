4b64775f41a306ef5d303f8638421db88cb734c3
Leland_Jin <nli32@ucsc.edu>
2021-05-02 23:25:58 +0800

    better looking menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -28,11 +28,9 @@ class Menu extends Phaser.Scene {
     }
     this.add.image(410, 250, 'menu_background');
     // show menu text
-    this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 'Endless Runner!', menuConfig).setOrigin(0.5);
-    this.add.text(game.config.width / 2, game.config.height / 2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
     menuConfig.backgroundColor = '#00FF00';
     menuConfig.color = '#000';
-    this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding, 'Press ↑ to test jump.js', menuConfig).setOrigin(0.5);
+    this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding - 20, 'Press ↑ to Play', menuConfig).setOrigin(0.5);
 
     // define keys
     keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
